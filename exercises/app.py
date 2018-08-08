30c504b6b22f8f90ef5059ee0884f93cb032f173
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 10:29:00 +0300

    flask

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-	return render_template( "index.html")
+	#return render_template( "index.html")
+	players = ["Me" , "Myself" ,"I"]
+	return render_template( "index.html" , 
+		players = players,
+		like_same_sport = True)
+
+		
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
 
   
 
